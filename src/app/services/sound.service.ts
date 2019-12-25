ee990be5c48f39cfab6a213b3b7287602590afa7
Jingshao Chen <jingshaochen@gmail.com>
2019-12-24 22:07:16 -0600

    added all sounds and sound service

--- src/app/services/sound.service.ts
+++ src/app/services/sound.service.ts
@@ -1,37 +1,82 @@
 import { Injectable } from '@angular/core';
-import { Howl, Howler } from 'howler'
+import { Howl } from 'howler'
 
 @Injectable({
   providedIn: 'root'
 })
 export class SoundService {
-  sound = new Howl({
-    src: ['/assets/ra2.mp3'],
-    // autoplay: true,
-    loop: true,
-    volume: 0.5,
-    onend: function () {
-      console.log('Finished!');
-    },
-    onplayerror: function () {
-        this.sound.once('unlock', function () {
-        this.sound.play();
-      });
-    }
-  })
+  public backgroundMusic: Howl
 
-  constructor() { }
-  play() {
-    this.sound.play()
+  public sounds: { [key: string]: Howl } = {}
+  public soundNames: Array<string> = [
+    'isNight',		  // 天黑了
+    'isDay',		    // 天亮了
+    'closeEyes',		// 请闭眼
+    'openEyes',		  // 请睁眼
+    'everyone',		  // 大家
+    'wolves',		    // 狼人
+    'guard',		    // 守卫
+    'witch',		    // 女巫
+    'hunter',		    // 猎人
+    'prophet',		  // 预言家
+    'voteSheriff',	// 开始竞选警长 
+    'voteStart',		// 请投票
+    'voteStop',		  // 投票结束
+    'pleaseSpeak',	// 请发言
+    'lastword', 		// 请说遗言
+    'choose' 		    // 请选择
+  ]
+
+  constructor() {
+    this.backgroundMusic = new Howl({
+      src: ['/assets/ra2.mp3'],
+      loop: true,
+      volume: 0.25,
+      onplayerror: function () {
+        this.backgroundMusic.once('unlock', function () {
+          this.backgroundMusic.play();
+        });
+      }
+    })
+    this.soundNames.forEach(name => {
+      this.sounds[name] = new Howl({
+        src: [`/assets/${name}.mp3`],
+        loop: false
+      })
+    })
   }
-  stop() {
-    this.sound.stop()
+
+  playAsync(sound: Howl) {
+    return new Promise(function (resolve, reject) {
+      sound.on('end', function () {
+        resolve()
+      })
+      sound.once('playerror', function () {
+        sound.once('unlock', function () {
+          sound.play()
+        })
+      })
+      sound.play()
+    })
   }
-  toggle() {
-    if (this.sound.playing()) {
-      this.sound.stop()
+
+  async playSequence(names: Array<string>) {
+    for (let name of names) {
+      await this.playAsync(this.sounds[name])
+    }
+  }
+
+  playBackgroundMusic() {
+    this.backgroundMusic.play()
+  }
+  stopBackgroundMusic() {
+    this.backgroundMusic.stop()
+  }
+  toggleBackgroundMusic() {
+    if (this.backgroundMusic.playing()) {
+      this.backgroundMusic.stop()
     } else {
-      this.sound.play()
+      this.backgroundMusic.play()
     }
   }
 }
